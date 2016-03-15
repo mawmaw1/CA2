8c40cded1b54f7620d77027f379ff4335f597ad2
mawmaw1 <kontakt@magnuslarsen.dk>
2016-03-15 10:06:21 +0100

    Added facade

---
+++ CA2/src/java/facade/iFacade.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import entity.Company;
+import entity.Hobby;
+import entity.Person;
+import java.util.List;
+
+/**
+ *
+ * @author Magnus
+ */
+public interface iFacade {
+    public Person getPerson(String phoneNumber);
+    public Company getCompany(String phoneNumber);
+    public List<Person> getPersonsFromHobby(Hobby hobby);
+    public List<Person> getPersons(String zip);
+    public int getHobbiesCount(Hobby hobby);
+    public List<String> getAllZip();
+    public List<Company> getCompanyListWithMoreEmployees(int number);
+    
+}
