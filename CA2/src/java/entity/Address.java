8020f4006a6c233b383bbc19cf39e33ffa1291e1
ingimuller <ingimuller93@gmail.com>
2016-03-16 12:50:09 +0100

    Table fix (nice)

--- CA2/src/java/entity/Address.java
+++ CA2/src/java/entity/Address.java
@@ -14,6 +14,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 
 /**
